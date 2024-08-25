1cba48a5687c2b7279f1c938c35f80678c0a0f8c
soniavarela <svarelavila@gmail.com>
2024-08-25 12:20:59 +0200

    Ejercicio ex03

---
+++ ex03/tester.py
@@ -0,0 +1,19 @@
+from new_student import Student
+
+
+def main():
+    """Main function to test the Student class."""
+
+    try:
+        student = Student(name="Edward", surname="agle")
+        print(student)
+
+        student = Student(name="Edward", surname="agle", id="toto")
+        print(student)
+
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
+
+
+if __name__ == "__main__":
+    main()
